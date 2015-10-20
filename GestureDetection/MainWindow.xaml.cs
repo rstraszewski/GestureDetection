08c2197bce7f54b96998557ecc8d791b8b50c0d5
rstraszewski <rstraszewski@gmail.com>
2015-10-20 23:48:35 +0200

    Remove redundant directives

--- GestureDetection/MainWindow.xaml.cs
+++ GestureDetection/MainWindow.xaml.cs
@@ -1,27 +1,12 @@
 ﻿using Emgu.CV;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using Emgu.CV.BgSegm;
 using Emgu.CV.CvEnum;
-using Emgu.CV.Structure;
 using Emgu.CV.Utility;
 using Emgu.CV.VideoSurveillance;
 using GestureDetection.Extensions;
-using Point = System.Drawing.Point;
-using Size = System.Drawing.Size;
 
 namespace GestureDetection
 {
